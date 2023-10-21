6210cd79aed8361746c4a03eeb302f22ce1c05e2
PruthvirajHawale <hawalepruthvi12@gmail.com>
2023-10-22 00:09:05 +0530

    Implemented Interpolation, Data Binding, Event Binding to add Event component design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from 'src/server/server.component';
+import { ServerComponent } from 'src/app/server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule
